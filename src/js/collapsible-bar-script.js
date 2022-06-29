4e43263b20050d26dba715d06804a04eb093e18d
Mahdi Toussi <mahdi.toussi2002@hotmail.com>
2022-06-29 16:47:24 +0100

    Updating fixed title, added collapsable side bar + background for skills page

---
+++ src/js/collapsible-bar-script.js
@@ -0,0 +1,11 @@
+//Sourced from: https://www.w3schools.com/howto/howto_js_collapse_sidebar.asp
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
