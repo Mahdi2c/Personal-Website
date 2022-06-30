5b7fa4c9673eca945b102f90026110c355a5dc39
Mahdi Toussi <mahdi.toussi2002@hotmail.com>
2022-06-30 19:05:33 +0100

    Updating size of collapsible bar

--- src/js/collapsible-bar-script.js
+++ src/js/collapsible-bar-script.js
@@ -1,7 +1,7 @@
 //Sourced from: https://www.w3schools.com/howto/howto_js_collapse_sidebar.asp
 function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("mySidebar").style.width = "225px";
+  document.getElementById("main").style.marginLeft = "225px";
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
