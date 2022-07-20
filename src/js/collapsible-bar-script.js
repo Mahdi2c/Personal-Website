9089645b0205691ad59001168a2fa6e1da41a29c
Mahdi Toussi <mahdi.toussi2002@hotmail.com>
2022-07-20 17:47:13 +0100

    Made fixed title responsive, added background to projects page + more details about projects added

--- src/js/collapsible-bar-script.js
+++ src/js/collapsible-bar-script.js
@@ -1,7 +1,16 @@
 //Sourced from: https://www.w3schools.com/howto/howto_js_collapse_sidebar.asp
-function openNav() {
-  document.getElementById("mySidebar").style.width = "225px";
-  document.getElementById("main").style.marginLeft = "225px";
+function openNav() 
+{
+  var mq = window.matchMedia( "(max-width: 768px)" );
+  if (mq.matches) {
+    document.getElementById("mySidebar").style.width = "120px";
+    document.getElementById("main").style.marginLeft = "120px";
+  }
+  else {
+    document.getElementById("mySidebar").style.width = "225px";
+    document.getElementById("main").style.marginLeft = "225px";
+  }
+
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
